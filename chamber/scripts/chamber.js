276d8f33880e57d3f4ee9d2796b030ee22087b55
Anzehlat <Anzehlat@gmail.com>
2024-01-27 10:26:26 -0700

    added photos and wrote css and html code for images

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -1,5 +1,33 @@
 document.getElementById('lastModified').textContent = new Date(document.lastModified).toLocaleString();
 
+document.addEventListener('DOMContentLoaded', function () {
+    const images = document.querySelectorAll('.image');
+    images.forEach(img => {
+      img.innerHTML = `<img src="" data-src="${img.dataset.src}" alt="${img.dataset.caption}">`;
+    });
+  
+    const visitMessage = document.getElementById('visitMessage');
+    const visitCount = localStorage.getItem('visitCount');
+    const lastVisit = localStorage.getItem('lastVisit');
+  
+    if (visitCount === null) {
+      visitMessage.textContent = 'Welcome! Let us know if you have any questions.';
+    } else {
+      const currentTime = Date.now();
+      const timeDifference = currentTime - lastVisit;
+      const daysDifference = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+  
+      if (daysDifference === 0) {
+        visitMessage.textContent = 'Back so soon! Awesome!';
+      } else {
+        const dayPlural = daysDifference === 1 ? 'day' : 'days';
+        visitMessage.textContent = `You last visited ${daysDifference} ${dayPlural} ago.`;
+      }
+    }
+  
+    localStorage.setItem('lastVisit', Date.now());
+  });  
+
 const currentYear = new Date().getFullYear();
 document.getElementById('currentYear').textContent = currentYear;
 

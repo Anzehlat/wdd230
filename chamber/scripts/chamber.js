73702dfcab2d83d6a36424ab4eb61cab2bcf3e00
Anzehlat <Anzehlat@gmail.com>
2024-02-03 20:22:54 -0700

    added and changed js file

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -39,3 +39,31 @@ hamburgerButton.addEventListener('click', () => {
     navElement.classList.toggle('open');
     hamburgerButton.classList.toggle('open');
 });
+
+document.getElementById('currentYear').textContent = currentYear;
+document.getElementById('lastModified').textContent = 'Last Modified: ' + document.lastModified;
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Set current timestamp in the hidden field
+    const timestampField = document.getElementById('timestamp');
+    const currentTimestamp = new Date().toISOString();
+    timestampField.value = currentTimestamp;
+});
+
+hamburgerButton.addEventListener('click', () => {
+    navElement.classList.toggle('open');
+    hamburgerButton.classList.toggle('open');
+});
+
+const modeButton = document.querySelector("#mode");
+const main = document.querySelector("main");
+
+modeButton.addEventListener("click", () => {
+    main.classList.toggle('dark-mode');
+    
+    if (modeButton.textContent.includes("🕶️")) {
+        modeButton.textContent = "🔆";
+    } else {
+        modeButton.textContent = "🕶️";
+    }
+});
